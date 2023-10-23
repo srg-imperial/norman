b792091bd80f014fef050d6370a9261a104fec7a
Daniel Schemmel <daniel@schemmel.net>
2023-10-23 13:04:04 +0100

    split non-static local vars from their initializers always

--- src/transform/VarDecl.cpp
+++ src/transform/VarDecl.cpp
@@ -13,19 +13,33 @@
 #include <iterator>
 #include <string>
 
+namespace {
+	std::string split(clang::ASTContext* astContext, clang::VarDecl* varDecl) {
+		clang::QualType type = varDecl->getType().getDesugaredType(*astContext);
+		type.removeLocalConst();
+		clang::VarDecl* vd =
+		  clang::VarDecl::Create(*astContext, varDecl->getDeclContext(), clang::SourceLocation(), clang::SourceLocation(),
+		                         varDecl->getIdentifier(), type, nullptr, varDecl->getStorageClass());
+
+		return fmt::format(
+		  "{};\n{} = ({});", *vd, varDecl->getName(),
+		  clang::Lexer::getSourceText(clang::CharSourceRange::getTokenRange(varDecl->getInit()->getSourceRange()),
+		                              astContext->getSourceManager(), astContext->getLangOpts()));
+	}
+} // namespace
+
 std::optional<std::string> transform::transformVarDecl(clang::ASTContext* astContext, clang::VarDecl* varDecl) {
 	if(clang::Expr* init = varDecl->getInit()) {
 		if(varDecl->isFileVarDecl() || varDecl->isStaticLocal()) {
 			// in C file vars and static locals have to be constant initialized
 		} else if(varDecl->isLocalVarDecl()) {
-			if(checks::reference(init, varDecl)) {
-				clang::VarDecl* vd = clang::VarDecl::Create(
-				  *astContext, varDecl->getDeclContext(), clang::SourceLocation(), clang::SourceLocation(),
-				  varDecl->getIdentifier(), varDecl->getType(), varDecl->getTypeSourceInfo(), varDecl->getStorageClass());
-
-				return fmt::format("{};\n{} = ({});", *vd, varDecl->getName(),
-				                   clang::Lexer::getSourceText(clang::CharSourceRange::getTokenRange(init->getSourceRange()),
-				                                               astContext->getSourceManager(), astContext->getLangOpts()));
+			if(!llvm::isa<clang::InitListExpr>(init)) {
+				if(checks::reference(init, varDecl)) {
+					return {split(astContext, varDecl)};
+				} else {
+					// TODO: add a configuration switch
+					return {split(astContext, varDecl)};
+				}
 			}
 		}
 	}
