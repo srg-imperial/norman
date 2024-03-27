5bbdea855b55152766521ad8443f2c05397b980d
Daniel Schemmel <daniel@schemmel.net>
2024-03-27 15:17:57 +0000

    add simple (combined) format for shorter tests

--- test/runner.py
+++ test/runner.py
@@ -2,10 +2,24 @@
 import argparse
 import os
 from pathlib import Path
+import re
 import shutil
 from subprocess import run
 
 SCRIPT_LOCATION = os.path.dirname(os.path.realpath(__file__))
+
+def parse_test(test):
+  with open(test, "r", encoding="utf-8") as f:
+    data = f.read()
+  match = re.fullmatch(r"(?:(.*?)\n)?---(\n.*?)(?:\n---\n(.*))?", data, flags=re.DOTALL)
+  if match is None:
+    print(f"Cannot parse test file `{test}`")
+    os._exit(1)
+  return (
+    "" if match.group(1) is None else match.group(1).strip(),
+    match.group(2).strip(),
+    None if match.group(3) is None else match.group(3).strip(),
+  )
 
 def main():
   parser = argparse.ArgumentParser()
@@ -15,23 +29,55 @@ def main():
   parser.add_argument("test", type=Path, help="test case id")
 
   args = parser.parse_args()
-  if args.test.is_absolute():
-    print("The test id must be relative path (e.g., `NullStmt/basic`)")
+
+  test = args.test
+  test_source = SCRIPT_LOCATION / test
+  if test.is_absolute():
+    test = test.resolve().relative_to(SCRIPT_LOCATION)
+  if not test_source.exists():
+    print(f"Test `{SCRIPT_LOCATION / test}` does not exist in the filesystem")
     os._exit(1)
 
-  shutil.rmtree(args.test, ignore_errors=True)
-  os.makedirs(args.test.parent, exist_ok=True)
-  shutil.copytree(SCRIPT_LOCATION / args.test, args.test)
+  shutil.rmtree(test, ignore_errors=True)
+  os.makedirs(test, exist_ok=True)
+
+  if test_source.is_dir():
+    actual = test / "actual.c"
+    shutil.copyfile(SCRIPT_LOCATION / test / "input.c", actual)
+
+    expected = test / "expected.c"
+    shutil.copyfile(SCRIPT_LOCATION / expected, expected)
+
+    config = test / "config.json"
+    if (SCRIPT_LOCATION / config).is_file():
+      shutil.copyfile(SCRIPT_LOCATION / config, config)
+    else:
+      config = None
+  else:
+    parts = parse_test(test_source)
+
+    actual = test / "actual.c"
+    with open(actual, "w", encoding="utf-8") as f:
+      f.write(parts[0])
+
+    expected = test / "expected.c"
+    with open(expected, "w", encoding="utf-8") as f:
+      f.write(parts[1])
+
+    config = None
+    if parts[2] is not None:
+      config = test / "config.json"
+      with open(config, "w", encoding="utf-8") as f:
+        f.write(parts[2])
 
   # norman currently does not support the `--` syntax due to option parser limitations...
-  if (args.test / "config.json").exists():
-    run([args.norman, "--config", args.test / "config.json", args.test / "input.c"], check=True)
+  if config is not None:
+    run([args.norman, "--config", config, actual], check=True)
   else:
-    run([args.norman, args.test / "input.c"], check=True)
-  os.rename(args.test / "input.c", args.test / "actual.c")
+    run([args.norman, actual], check=True)
 
-  run([args.clang_format, "-i", "--", args.test / "expected.c", args.test / "actual.c"], check=True)
-  result = run(["diff", "--minimal", "--", args.test / "expected.c", args.test / "actual.c"])
+  run([args.clang_format, "-i", "--", expected, actual], check=True)
+  result = run(["diff", "--minimal", "--", expected, actual])
   os._exit(result.returncode)
 
 if __name__ == '__main__':
